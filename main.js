8ab66df5f2cca31f9dcc406b28a18f29980e0f32
Jisu <kjl0349@naver.com>
2021-01-18 02:00:01 +0900

    [js] mouseHover 함수 정의, hover 된 요소가 active 클래스를 가지고 있지 않을 때 다른 리스트들에서 active 클래스를 제거하고 투명도 부여

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 const target = document.querySelector('.target');
 const links = document.querySelectorAll('.mynav a');
+const colors = ['#1abc9c', '#27ae60', '#3498db', '#8e44ad', '#f1c40f', '#e67e22', '#e74c3c', '#2c3e50'];
 
 for(let i = 0; i < links.length; i++){
     links[i].addEventListener('click', e => e.preventDefault());
     links[i].addEventListener('mouseenter', mouseHover);
 }
 
+function mouseHover() {
+    if(!this.parentNode.classList.contains('active')) {
+        for(let i = 0; i < links.length; i++) {
+            if(links[i].parentNode.classList.contains('active')) {
+                links[i].parentNode.classList.remove('active');
+            }
+            links[i].style.opacity = '.3';
+        }
+        this.parentNode.classList.add('active');
+        this.style.opacity = '1';
+
+    }
+}
